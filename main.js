4544a50ce2439056a8b21bd05ebb2984afc78ff2
Matthew Schneider <59853199+mpschneider2@users.noreply.github.com>
2025-02-18 02:19:19 -0800

    get arrow keys working

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-const keyMap = {
-    "a": 0x04, "b": 0x05, "c": 0x06, "d": 0x07,
-    "e": 0x08, "f": 0x09, "g": 0x0A, "h": 0x0B,
-    "i": 0x0C, "j": 0x0D, "k": 0x0E, "l": 0x0F,
-    "m": 0x10, "n": 0x11, "o": 0x12, "p": 0x13,
-    "q": 0x14, "r": 0x15, "s": 0x16, "t": 0x17,
-    "u": 0x18, "v": 0x19, "w": 0x1A, "x": 0x1B,
-    "y": 0x1C, "z": 0x1D,
-
-    "0": 0x27, "1": 0x1E, "2": 0x1F, "3": 0x20,
-    "4": 0x21, "5": 0x22, "6": 0x23, "7": 0x24,
-    "8": 0x25, "9": 0x26,
-
-    "Enter": 0x28, "Escape": 0x29, "Backspace": 0x2A, "Tab": 0x2B,
-    " ": 0x2C, "-": 0x2D, "=": 0x2E, "[": 0x2F, "]": 0x30,
-    "\\": 0x31, ";": 0x33, "'": 0x34, "`": 0x35, ",": 0x36,
-    ".": 0x37, "/": 0x38,
-
-    "ArrowUp": 0x52, "ArrowDown": 0x51, "ArrowLeft": 0x50, "ArrowRight": 0x4F
-};
-
 function formatShortcut(keys) {
     return [...keys].map(key => key.replace(" ", "Space")).join(" + ");
 }
@@ -54,7 +33,7 @@ class InputBox {
         console.log(event.key);
         this.pressedKeys.add(event.key);
         // this.setKeys.add(event.key.toLowerCase().charCodeAt());
-        if (!["Alt", "Ctrl", "Meta", "Shift", "Tab"].includes(event.key)) {
+        if (!["Alt", "Ctrl", "Meta", "Shift", "Tab", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"].includes(event.key)) {
             this.setKeys.add(event.key.toLowerCase().charCodeAt());
         } else {
             if(event.key == "Alt") this.setKeys.add(130);
@@ -62,6 +41,10 @@ class InputBox {
             if(event.key == "Meta") this.setKeys.add(135);
             if(event.key == "Shift") this.setKeys.add(129);
             if(event.key == "Tab") this.setKeys.add(9);
+            if(event.key == "ArrowLeft") this.setKeys.add(216);
+            if(event.key == "ArrowRight") this.setKeys.add(215);
+            if(event.key == "ArrowUp") this.setKeys.add(218);
+            if(event.key == "ArrowDown") this.setKeys.add(217);
         }
         this.box.textContent = formatShortcut(this.pressedKeys);
     }
